15a053e778fb5f5568f3ef4111a6b2fb08d0beb8
Vladimir V. Myagdeev <concertbil@gmail.com>
2017-01-16 01:00:52 +0000

    First commit

---
+++ config/prod.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__.'/secured/db.php';
+
+// configure your app for the production environment
+
+$app['twig.path'] = array(__DIR__.'/../templates');
+$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');

d1f1ee6a39691c5f9276c43c3c625d60ca77003e
Vladimir V. Myagdeev <concertbil@gmail.com>
2017-01-16 01:09:37 +0000

    Disabled twig Cache

--- config/prod.php
+++ config/prod.php
@@ -4,4 +4,4 @@ require __DIR__.'/secured/db.php';
 // configure your app for the production environment
 
 $app['twig.path'] = array(__DIR__.'/../templates');
-$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['twig.options'] = array('cache' => false);
